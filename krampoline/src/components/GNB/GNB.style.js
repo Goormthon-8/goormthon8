3be08bfe862b4d4aff2f6507d1e335c05271fe1c
Wang HoEun <hoeunwang@gmail.com>
2023-11-30 15:26:37 +0900

    style: css 수정 및 router 코드 작성

--- krampoline/src/components/GNB/GNB.style.js
+++ krampoline/src/components/GNB/GNB.style.js
@@ -16,7 +16,7 @@ export const ButtonContainer = styled.div`
   align-items: center;
   width: 100%;
   height: 100%;
-  padding: 2rem;
+  padding: 1.5rem;
 
   color: ${({ $nowPage }) => getButtonColor($nowPage)};
   &:hover {
