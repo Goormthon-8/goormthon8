14931e3a58da4afaaf65160a2f1b92e8e123f387
Wang Hoeun <38005874+hoeun0723@users.noreply.github.com>
2023-11-30 17:37:25 +0900

    날짜 선택 레이아웃 요소 구성 완료 (#7)
    * feat: 컴포넌트 페이지 생성 및 버튼 생성 완료
    
    * feat: gnb 제작
    
    * feat: input 컴포넌트 생성
    
    * refactor: main page 초기화
    
    * feat: ButtonContainer 제작 => 수정 필요
    
    * feat: ButtonContainer 제작 및 시작페이지 제작
    
    * feat: AI PAGE style 제작
    
    * feat: api연결 코드 형태 작성
    
    * feat: 버튼에 onClick 추가
    
    * feat: env 파일 git ignore에 생성
    
    * feat: recoil 세팅
    
    * style: css 수정 및 router 코드 작성
    
    * feat: Theme select 페이지 필요요소 생성
    
    * feat: 날짜 선택 페이지 레이아웃 구성
    
    ---------
    
    Co-authored-by: Wang HoEun <hoeunwang@gmail.com>

--- krampoline/src/Router.jsx
+++ krampoline/src/Router.jsx
@@ -4,6 +4,7 @@ import StickerPage from "./pages/Sticker";
 import Start from "./pages/Start";
 import StampMake from "./pages/StampMake";
 import Main from "./pages/Main";
+import ThemeSelect from "./pages/ThemeSelect";
 
 const Router = () => {
   return (
@@ -14,6 +15,9 @@ const Router = () => {
         <Route path="/test" element={<Start />} />
         <Route path="/stampMake" element={<StampMake />} />
         <Route path="/test" element={<Main />} />
+        <Route path="/" element={<Start />} />
+        <Route path="/theme-select" element={<ThemeSelect />} />
+        <Route path="/stamp-make" element={<StampMake />} />
         <Route path="/sticker" element={<StickerPage />} />
       </Routes>
     </BrowserRouter>
