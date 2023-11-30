259ffd3b1f31609d145643e51b0d87663878bc1f
Wang HoEun <hoeunwang@gmail.com>
2023-12-01 05:42:15 +0900

    feat: 유저등록 api 연결

--- krampoline/src/pages/Start/index.jsx
+++ krampoline/src/pages/Start/index.jsx
@@ -5,20 +5,20 @@ import Input from "../../components/Input";
 import axios from "axios";
 import { useState } from "react";
 const Start = () => {
-  //  const navigate = useNavigate();
+  const navigate = useNavigate();
   const [username, setUsername] = useState("");
 
   const handleInput = (e) => {
     setUsername(e.target.value);
   };
   const handleButton = async () => {
-    // navigate("./make-new");
-    const formData = { username };
-
-    const API_URL = import.meta.env.REACT_BASE_URL;
-    const response = await axios.post(`${API_URL}/api/users`, formData);
+    const API_URL = process.env.REACT_APP_BASE_URL;
+    const response = await axios.post(`${API_URL}/api/users`, {
+      username: username,
+    });
     console.log("클릭");
     console.log(response);
+    navigate("/make-new");
   };
   return (
     <S.Container>
