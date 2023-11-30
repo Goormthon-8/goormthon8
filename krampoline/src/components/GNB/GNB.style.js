a80be3faed301dcd5c7e9cd1f390dacb92ae6f51
Wang HoEun <hoeunwang@gmail.com>
2023-11-30 10:20:00 +0900

    feat: gnb 제작

--- krampoline/src/components/GNB/GNB.style.js
+++ krampoline/src/components/GNB/GNB.style.js
@@ -4,5 +4,22 @@ export const Container = styled.div`
   display: flex;
   justify-content: space-around;
   align-items: center;
-  width: 100%;
+  width: 36rem;
+  border: 1px solid;
+`;
+
+const getButtonColor = (nowPage) => (nowPage ? "black" : "gray");
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 12rem;
+  height: 100%;
+  padding: 2rem;
+
+  color: ${({ $nowPage }) => getButtonColor($nowPage)};
+  &:hover {
+    background-color: gray;
+  }
 `;
--- krampoline/src/pages/Main/index.jsx
+++ krampoline/src/pages/Main/index.jsx
@@ -1,10 +1,12 @@
 import Button from "../../components/Button";
+import GlobalNavigation from "../../components/GNB";
 
 const Main = () => {
   return (
     <>
       <h1>확인!</h1>
       <Button>입력하기</Button>
+      <GlobalNavigation nowPage={2} />
     </>
   );
 };
