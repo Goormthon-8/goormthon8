315ec2624dba43c4456c3e0d91332a890ad890f1
Wang HoEun <hoeunwang@gmail.com>
2023-11-30 10:25:51 +0900

    refactor: main page 초기화

--- krampoline/src/pages/Main/index.jsx
+++ krampoline/src/pages/Main/index.jsx
@@ -1,14 +1,7 @@
-import Button from "../../components/Button";
-import GlobalNavigation from "../../components/GNB";
-import Input from "../../components/Input";
-
 const Main = () => {
   return (
     <>
       <h1>확인!</h1>
-      <Button>입력하기</Button>
-      <GlobalNavigation nowPage={2} />
-      <Input placeholder="입력해주세요" />
     </>
   );
 };
