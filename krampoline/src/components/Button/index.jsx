618aab1503d86b9d4a22b32a5dfd5f3dacefcf80
innerstella <suji0509@naver.com>
2023-11-30 14:50:59 +0900

    feat : sticker

--- krampoline/src/components/Button/index.jsx
+++ krampoline/src/components/Button/index.jsx
@@ -2,8 +2,8 @@ import * as S from "./Button.style";
 
 const Button = ({ children, disabled, onClick }) => {
   return (
-    <S.Container disabled={disabled}>
-      <S.Text>{children}</S.Text>
+    <S.Container disabled={disabled} onClick={onClick}>
+      {children}
     </S.Container>
   );
 };
